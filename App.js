92e623e19a680050b8acbeeb061c7c024c2d43c3
evenfrost <evenfrosty@gmail.com>
2018-01-30 19:58:04 +0300

    Add some state

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
-export default () => (
-  <div className="h-screen w-screen flex justify-center items-center" >
-    <div className="btn-group">
-      <button class="btn btn-primary">Primary</button>
-      <button class="btn btn-success">Success</button>
-    </div>
-  </div>
-);
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state.buttonTitle = 'Primary';
+  }
+
+  handleButtonClick() {
+    this.setState({
+      buttonTitle: 'Clicked',
+    });
+  }
+
+  render() {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center" >
+        <div className="btn-group">
+          <button class="btn btn-primary" onClick={event => this.handleButtonClick()}>{this.state.buttonTitle}</button>
+          <button class="btn btn-success">Success</button>
+        </div>
+      </div>
+    );
+  }
+};
